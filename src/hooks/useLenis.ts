da7632a2fd690224f24d613675cf7ad2db1ef74d
Thi Nguyen <dakthi@This-MacBook-Air.local>
2025-08-01 09:17:15 +0100

    lenis

--- src/hooks/useLenis.ts
+++ src/hooks/useLenis.ts
@@ -4,9 +4,9 @@ import Lenis from '@studio-freight/lenis';
 export const useLenis = () => {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 2.0, // Much slower, more natural scrolling
+      duration: 1.0, // Much slower, more natural scrolling
       easing: (t: number) => 1 - Math.pow(1 - t, 3), // Gentler easing curve
-      wheelMultiplier: 0.5, // Much less aggressive wheel response
+      wheelMultiplier: 0.8, // Much less aggressive wheel response
       touchMultiplier: 0.8, // Slower touch response
     });
 
